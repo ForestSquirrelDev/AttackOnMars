b9309f0dd36af05a803c947490c5cbc3a52fb87f
Lev Zhybulia <l.zhybulia@kefirgames.com>
2022-01-30 17:10:26 +0300

    Add grid

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game {
     public class GridManager : MonoBehaviour {
+        public int width = 20;
+        public int height = 20;
+        public float cellSize = 3;
+        public Transform cube;
         
+        private Grid grid;
+        
+        private void Awake() {
+            grid = new Grid(width, height, cellSize, transform);
+            Debug.Log(grid.WorldToGridCell(cube.position));
+        }
     }
 }
