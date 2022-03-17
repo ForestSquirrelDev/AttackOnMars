d300fe875b53515f8f55f4e6391861fc3fa3f69a
Lev Zhybulia <82777171+ForestSquirrelDev@users.noreply.github.com>
2022-03-18 00:23:40 +0300

    Remove old unused code

--- Assets/Scripts/Game/Ecs/Components/Tags/Tag_BuildingGhost.cs
+++ Assets/Scripts/Game/Ecs/Components/Tags/Tag_BuildingGhost.cs
@@ -1,8 +1,9 @@
+using Shared;
 using Unity.Entities;
 
 namespace Game.Ecs.Components {
     [GenerateAuthoringComponent]
     public struct Tag_BuildingGhost : IComponentData {
-        
+        public BuildingType BuildingType;
     }
 }
--- Assets/Scripts/Game/Ecs/Systems/SpawnBuildingSystem.cs
+++ Assets/Scripts/Game/Ecs/Systems/SpawnBuildingSystem.cs
@@ -14,16 +14,16 @@ namespace Game.Ecs.Systems {
     public partial class SpawnBuildingSystem : SystemBase {
         private GridKeeperSystem _gridKeeperSystem;
         private Camera _camera;
-        private EndSimulationEntityCommandBufferSystem _endSimulationECB;
+        private EndSimulationEntityCommandBufferSystem _endSimulationEcb;
         
         protected override void OnCreate() {
             _gridKeeperSystem = World.GetOrCreateSystem<GridKeeperSystem>();
             _camera = Camera.main;
-            _endSimulationECB = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+            _endSimulationEcb = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
         }
 
         protected override void OnUpdate() {
-            var ecb = _endSimulationECB.CreateCommandBuffer();
+            EntityCommandBuffer ecb = _endSimulationEcb.CreateCommandBuffer();
             Entities.WithAll<Tag_AvailableForPlacementGhostQuad>().ForEach((ref Parent parent) => {
                 if (!Input.GetMouseButtonDown(0)) return;
                 TrySpawnBuilding(EntityManager.GetComponentData<BuildingGhostComponent>(parent.Value).BuildingType);
@@ -32,14 +32,8 @@ namespace Game.Ecs.Systems {
         }
         
         private bool TrySpawnBuilding(BuildingType type) {
-            if (!_gridKeeperSystem.buildingGrid.InstantiateOnGrid(InputUtility.MouseToWorld(_camera),
-                ConvertedEntitiesContainer.Entities[type].building, EntityManager, out Entity building)) return false;
-            Entity positioningQuad = building.ReverseFindEntityWithComponent<PositioningQuadComponent>(EntityManager);
-            EntityManager.AddBuffer<Int2BufferElement>(positioningQuad);
-            InstantiatedBuildingsContainer.SpawnedBuilding spawnedBuilding = new InstantiatedBuildingsContainer.SpawnedBuilding 
-                { buildingRoot = building, positioningQuad = positioningQuad };
-            InstantiatedBuildingsContainer.Buildings.Add(spawnedBuilding);
-            return true;
+            return _gridKeeperSystem.buildingGrid.InstantiateOnGrid(InputUtility.MouseToWorld(_camera),
+                ConvertedEntitiesContainer.Entities[type].building, EntityManager, out _);
         }
     }
 }
